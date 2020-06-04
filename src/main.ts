2fd06525e891762128a2e557c44c52d239a7fdf8
EliazTray <944126009yh@gmail.com>
2020-06-04 22:31:42 +0800

    chore: add element-ui

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,19 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
+import Cookies from 'js-cookie'
+
+import 'normalize.css/normalize.css' // a modern alternative to CSS resets
+
+import Element from 'element-ui'
+import './styles/element-variables.scss'
+
+Vue.use(Element, {
+    size: Cookies.get('size') || 'medium' // set element-ui default size
+})
+
+import '@/styles/index.scss' // global css
+
 Vue.config.productionTip = false
 
 new Vue({
